6ad29e08fdf4ac76c27332ee4e7eb35c5c67b87c
Anshkumar01 <chaudharyansh0123@gamil.com>
2025-07-21 13:04:51 +0530

    my-portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom"; 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
- <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-
+  </React.StrictMode>
 );
