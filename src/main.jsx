a4677b8a3d15a41199d015b886767d704f153e2f
Anshkumar01 <chaudharyansh0123@gamil.com>
2025-07-21 12:24:26 +0530

    my-portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom"; // ✅ use HashRouter
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>
+);
